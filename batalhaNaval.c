8b5a4c9eb0da88a499f5240aa2f2a167960640cd
BrStahl <139174876+BrStahl@users.noreply.github.com>
2025-09-02 19:58:30 +0000

    Criaçao do Tabuleiro e inicializado os Navios.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,42 +4,127 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    int Tabuleiro[10][10] ={
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0}
-    };
+// Função que valida se ja possui navio no local.
+int verificaposicao(int linha,int coluna,int tamanho,int orientacao, int Tab[10][10]){
+    if(orientacao == 0){
+        for(int aux = coluna; aux < coluna+tamanho;aux++){
+            if(Tab[linha][aux] != 0 ){
+                return 0; 
+            }
+        }
+    } else{
+        for (int aux = linha;aux < linha+tamanho;aux++){
+            if(Tab[aux][coluna] != 0){
+                return 0;
+            }
+        }
+    }
+}
 
+int main() {
+
+    // Variaveis de Agua,Navio,Tamanho Tabuleiroe e Tabuleiro 10x10
+    int Tamanho_Tabu = 10;
+    int Tabuleiro[Tamanho_Tabu][Tamanho_Tabu];
+    int Agua = 0;
+    int Navio = 3;
+
+    // Variaveis do indice de Linha (1 - 10) e do Indice de Coluna (A - J)
     char Colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
     int Linha[10] ={1,2,3,4,5,6,7,8,9,10};
 
-    printf("-------Tabuleiro Batalha Naval-----\n");
+    // Inicializa o Tabuleiro do jogo 10x10
+    for (int linha = 0; linha < 10; linha++){
+        for(int coluna = 0; coluna <10; coluna++){
+            Tabuleiro[linha][coluna] = Agua;
+        }
+    }
+
+    printf("-------Legandas Tabuleiro-------\n");
+
+    printf("\nLegenda\n");
+    printf("\nÁgua = 0\n");
+    printf("Navio = 3\n");
+    
+    printf("\n   ");
+
+    printf("\n-------Tabuleiro Batalha Naval-------\n");
+    printf("\n   ");
+
+
+    //Variaveis do Navio 1 (Horizontal) no Tabuleiro.
+    int Tamanho_Navio1 = 4;
+    int Linha_navio1 = 1;
+    int Coluna_navio1 = 2;
+    int Orientacao_Navio1 = 0; // 0 - Horizontal e 1 Vertical
+    
+    // Verificad se não excede o tamanho do Tabuleiro
+    if(Tamanho_Navio1 + Coluna_navio1 > 10){
+        printf("Navio não foi posicionado!\n");
+    } else { 
+        if(!validaposicao(Linha_navio1,Coluna_navio1,Tamanho_Navio1,Orientacao_Navio1,Tabuleiro)){ // Chama função de validação de posição
+            printf("ERRO: Navio não posicionado, pois ja possui um navio na posição desejada!\n");
+            return 0;
+        } else { // Inicializa o Navio 1 no tabuleiro
+            for(int indice = Coluna_navio1 ;indice < Coluna_navio1+Tamanho_Navio1;indice++){
+                Tabuleiro[Linha_navio1][indice] = Navio;
+            }
+        }
+    }
 
     printf("\n   ");
+
+    //Variaveis do Navio 2 (Horizontal) no Tabuleiro.
+    int Tamanho_Navio2 = 7;
+    int Linha_navio2 = 2;
+    int Coluna_navio2 = 8;
+    int Orientacao_Navio2 = 1; // 0 - Horizontal e 1 Vertical
     
+    // Verificad se não excede o tamanho do Tabuleiro
+    if(Tamanho_Navio2 + Linha_navio2 > 10){
+        printf("Navio não foi posicionado!\n");
+    } else {
+        if(!verificaposicao(Linha_navio2,Coluna_navio2,Tamanho_Navio2,Orientacao_Navio2,Tabuleiro)){ // Chama função de validação de posição
+            printf("ERRO: Navio não posicionado, pois ja possui um navio na posição desejada!\n");
+            return 0;
+        } else { // Inicializa o Navio 2 no tabuleiro
+            for(int indice = Linha_navio2 ;indice < Linha_navio2+Tamanho_Navio2;indice++){
+                Tabuleiro[indice][Coluna_navio2] = Navio;
+            }
+        }
+
+    }
+
+    printf("\n   ");
+
+    //Exibição dos Indices da Coluna
     for (int aux = 0; aux < 10; aux++){
-        printf("%c | ",Colunas[aux]);
+        printf(" %c  ",Colunas[aux]);
     }
 
     printf("\n");
 
+
     //Exibe Tabuleiro 
     for (int linha = 0; linha < 10;linha++){
-        printf("%d  ",Linha[linha]);
-        for(int coluna = 0; coluna < 10;coluna++){
-            printf("%d | ",Tabuleiro[linha][coluna]);
+    
+        //Exibição dos Indices da Coluna
+        if(linha < 9){
+            printf("%d   ",Linha[linha]);
+            //Exibição dos Indices da Coluna
+            for(int coluna = 0; coluna < 10;coluna++){
+                printf("%d | ",Tabuleiro[linha][coluna]);
+            }
+                printf("\n");
+        } else{
+            printf("%d  ",Linha[linha]);
+            for(int coluna = 0; coluna < 10;coluna++){
+                printf("%d | ",Tabuleiro[linha][coluna]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
-
+        printf("------------------------------------------\n");
     return 0;
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -66,6 +151,4 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
 }
